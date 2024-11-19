9ed56cdaac4bddad49b3a5237f462f7ec3964e1d
Kanit Bunyinkgool <120672560+wavekanit@users.noreply.github.com>
2024-11-19 09:27:09 +0000

    fix: some bug

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_neg_age_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-2), "error age input")
+
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(35), 150)
+
+    def test_old_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(63), 100)
 
 if __name__ == '__main__':
     unittest.main()
